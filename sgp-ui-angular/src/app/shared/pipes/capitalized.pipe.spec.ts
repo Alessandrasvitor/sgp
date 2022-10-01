d695dca422879eb0232ed9ba334cec648109f969
alessandrasilvav@gmail.com <Programa25>
2022-09-30 22:16:32 -0300

    Inclusão de menu e configuração do  CanActivate

---
+++ sgp-ui-angular/src/app/shared/pipes/capitalized.pipe.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { CapitalizedPipe } from './capitalized.pipe';
+
+describe('Pipe: Capitalizede', () => {
+  it('create an instance', () => {
+    let pipe = new CapitalizedPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ sgp-ui-angular/src/app/shared/pipes/menu.pipe.ts
@@ -0,0 +1,19 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'menu'
+})
+export class MenuPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): any {
+    switch ( value ) {
+      case 'USER':
+        return {label: 'Usuários', icon: 'pi pi-users', routerLink: '/user'};
+      case 'COURSE':
+        return {label: 'Curso', icon: 'pi pi-book', routerLink: '/course'};
+      case 'BASIC':
+        return {label: 'Instituição', icon: 'pi pi-globe', routerLink: '/instituition'};
+    }
+  }
+
+}
